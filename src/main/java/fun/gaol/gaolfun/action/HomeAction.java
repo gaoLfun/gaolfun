43b3092713c163d0b79b98c85a3d1d0bf51c3518
gaoL <450569937@qq.com>
2019-11-14 10:15:34 +0800

    首页获取文章

--- src/main/java/fun/gaol/gaolfun/action/HomeAction.java
+++ src/main/java/fun/gaol/gaolfun/action/HomeAction.java
@@ -1,12 +1,22 @@
 package fun.gaol.gaolfun.action;
 
+import fun.gaol.gaolfun.action.index.bo.IndexManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeAction {
+    @Autowired
+    private IndexManager indexManager;
     @RequestMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Map<String, Object>> list_article = indexManager.getArticle();
+        model.addAttribute("list_article",list_article);
         return "Index";
     }
 }
